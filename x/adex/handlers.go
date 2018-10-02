72f067d61f4397a5386442117d79e4a710a0fd3a
Ivo Georgiev <ivo@adex.network>
2018-10-02 19:20:22 +0300

    scaffold ClaimTokenMsg

--- x/adex/handlers.go
+++ x/adex/handlers.go
@@ -2,12 +2,23 @@ package adex
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	types "github.com/cosmos/cosmos-sdk/adex/x/adex/types"
 	"github.com/cosmos/cosmos-sdk/x/bank"
 )
 
 func NewHandler(k bank.Keeper) sdk.Handler {
 	return func(ctx sdk.Context, msg sdk.Msg) sdk.Result {
-			errMsg := "Unrecognized adex Msg type"
-			return sdk.ErrUnknownRequest(errMsg).Result()
+		switch msg := msg.(type) {
+			case types.ClaimTokenMsg:
+				return handleClaimToken(k, ctx, msg)
+			default:
+				errMsg := "Unrecognized adex Msg type"
+				return sdk.ErrUnknownRequest(errMsg).Result()
+		}
 	}
+}
+
+func handleClaimToken(k bank.Keeper, ctx sdk.Context, msg sdk.Msg) sdk.Result {
+	//k.AddCoins(ctx, submitter, coins)
+	return sdk.Result{}
 }
---
+++ x/adex/types/claimTokenMsg.go
@@ -0,0 +1,51 @@
+package types
+
+import (
+	"encoding/json"
+	"fmt"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+type ClaimTokenMsg struct {
+	Amount	int64	`json:"amount"`
+	// @TODO ethSig, ethAddr; the ethSig must be signing the cosmos addr of the requester
+}
+
+// Implement msg
+func (msg ClaimTokenMsg) Type() string {
+	return "adex"
+}
+
+// Get Implements Msg
+func (msg ClaimTokenMsg) Get(key interface{}) (value interface{}) {
+	return nil
+}
+
+// GetSignBytes Implements Msg
+func (msg ClaimTokenMsg) GetSignBytes() []byte {
+	b, err := json.Marshal(msg)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+// GetSigners Implements Msg
+func (msg ClaimTokenMsg) GetSigners() []sdk.AccAddress {
+	return nil
+}
+
+// ValidateBasic Implements Msg
+func (msg ClaimTokenMsg) ValidateBasic() sdk.Error {
+	if msg.Amount < 0 {
+		return sdk.ErrInvalidCoins("invalid amount")
+	}
+
+	return nil
+}
+
+func (msg ClaimTokenMsg) String() string {
+	return fmt.Sprintf("ClaimTokenMsg{%v}", msg.Amount)
+}
+
+// @TODO: implement the msg interface
